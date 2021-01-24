53bcda43500dcab54986c7e39de1666264ee942d
osxzhg <osxzhg@gmail.com>
2021-01-24 21:49:59 +1300

    add find intersection of two strings

---
+++ FindIntersection/FindIntersectionService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindIntersection
+{
+    public class FindIntersectionService
+    {
+        public static string FindIntersection(string[] strArr)
+        {
+            List<string> sList=ParseInput(strArr[0]);
+            List<string> dList=ParseInput(strArr[1]);
+            var result= sList.Intersect(dList);
+            // code goes here  
+            return result.Count()>0 ? string.Join(",", sList.Intersect(dList)) : "false";
+
+        }
+
+        public static List<string> ParseInput(string str)
+        {
+            return str.Split(',').Select(a => a.TrimStart()).ToList();
+        }
+    }
+}
