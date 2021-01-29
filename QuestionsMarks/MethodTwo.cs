f0297a762f8936111f289f6c4664e0351359906a
osxzhg <osxzhg@gmail.com>
2021-01-29 21:56:32 +1300

    add MethodTwo which can detection d???d

---
+++ QuestionsMarks/MethodTwo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace QuestionsMarks
+{
+    public class MethodTwo
+    {
+        // replace all character to empty, except numbers and question marks
+        // find all d???d, each character use once
+        // check find at least one 
+        // check each of sum of two is 10
+        public static string QuestionsMarks(string str)
+        {
+            string polished = Regex.Replace(str, @"[^\d\?]", string.Empty);
+            var matches = Regex.Matches(polished, @"([\d]{1}[\?]{3}[\d]{1})");
+            var results = new List<int>();
+
+            foreach (Match m in matches)
+            {
+                //int a = Convert.ToInt32(m.Value.First().ToString());
+                int a = Convert.ToInt32(m.Value.First().ToString()); // char to int will get the unicode
+                int b = Convert.ToInt32(m.Value.Last().ToString()); // string to int will get the decimal
+
+                results.Add(a + b);
+            }
+
+            string result = results.Count != 0 && results.All(r => r == 10) ? "true" : "false";
+            return result;
+        }
+    }
+}
---
+++ QuestionsMarksTest/MethodTwoQuestionsMarks.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using QuestionsMarks;
+
+namespace QuestionsMarksTest
+{
+    public class MethodTwoQuestionsMarks
+    {
+        [Theory]
+        [InlineData("acc?7??sss?3rr1??????5", "true")]
+        public void Success(string str1, string expected)
+        {
+
+            var result = MethodTwo.QuestionsMarks(str1);
+            Assert.Equal(expected, result);
+
+        }
+        [Theory]
+        [InlineData("9???1???9??1???9", "true")]
+        public void OneNumberUseTwice(string str1, string expected)
+        {
+
+            var result = MethodTwo.QuestionsMarks(str1);
+            Assert.Equal(expected, result);
+
+        }
+
+        [Theory]
+        [InlineData("9???23???7", "false")]
+        public void SumIsNotTen(string str1, string expected)
+        {
+
+            var result = MethodTwo.QuestionsMarks(str1);
+            Assert.Equal(expected, result);
+
+        }
+    }
+}
