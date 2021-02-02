a5bb42d1a67bcc34f6d4ec9fbcb71d75869cb408
osxzhg <osxzhg@gmail.com>
2021-02-03 10:53:22 +1300

    add method two for longest increasing sequence

---
+++ LengthOfLISTest/MethodTwoTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using LongestIncreasingSequence;
+
+namespace LengthOfLISTest
+{
+    public class MethodTwoTest
+    {
+        [Theory]
+        [InlineData(4, new int[] { 10, 9, 2, 5, 3, 7, 101, 18 })]
+        public void Test1(int expected, int[] nums)
+        {
+            var result = MethodTwo.LongestIncreasingSequence(nums);
+            Assert.Equal(expected, result);
+
+        }
+
+        [Theory]
+        [InlineData(8, new int[] { 100, 101, 102,105, 2,3,4,90,91,92,93,94 })]
+        public void LargeNumberBeforeSmallNumbers(int expected, int[] nums)
+        {
+            var result = MethodTwo.LongestIncreasingSequence(nums);
+            Assert.Equal(expected, result);
+
+        }
+    }
+}
+
---
+++ LongestIncreasingSequence/MethodTwo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LongestIncreasingSequence
+{
+    public class MethodTwo
+    {
+        public static int LongestIncreasingSequence(int[] arr)
+        {
+            int n = arr.Length;
+            int[] dp = new int[n];
+
+            int maxLen = 0;
+            foreach (int num in arr)
+            {
+                int index = Array.BinarySearch(dp, 0, maxLen, num);
+
+                if(index < 0)
+                {
+                    index = -(index + 1);
+                }
+
+                dp[index] = num;
+
+                if (index == maxLen)
+                {
+                    maxLen++;
+                }
+            }
+            return maxLen;
+        }
+
+    }
+}
