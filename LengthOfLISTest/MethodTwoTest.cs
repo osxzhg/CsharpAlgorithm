6a8b12c366c82e3b4b99ea4cb53b3f7d22328270
osxzhg <osxzhg@gmail.com>
2021-02-12 00:22:24 +1300

    add methodtwo for longest increasing sequence

--- LengthOfLISTest/MethodTwoTest.cs
+++ LengthOfLISTest/MethodTwoTest.cs
@@ -25,6 +25,32 @@ namespace LengthOfLISTest
             Assert.Equal(expected, result);
 
         }
+        [Theory]
+        [InlineData(8, new int[] { 2, 3, 4, 90, 91,92, 93, 94 },new int[] { 100, 101, 102, 105, 2, 3, 4, 90, 91, 92, 93, 94 })]
+        public void LisArray(int dummy, int[] expected,int[] nums)
+        {
+            var result = MethodTwo.LongestIncreasingSequenceArray(nums);
+            Assert.Equal(expected, result);
+
+        }
+
+        [Theory]
+        [InlineData(8, new int[] { 0, 2, 6, 9, 11, 15 }, new int[] { 0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15 })]
+        public void LisArrayWithZero(int dummy, int[] expected, int[] nums)
+        {
+            var result = MethodTwo.LongestIncreasingSequenceArray(nums);
+            Assert.Equal(expected, result);
+
+        }
+
+        [Theory]
+        [InlineData(8, new int[] { 2,6,9,11}, new int[] { 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7 })]
+        public void LisArrayWithDebug(int dummy, int[] expected, int[] nums)
+        {
+            var result = MethodTwo.LongestIncreasingSequenceArray(nums);
+            Assert.Equal(expected, result);
+
+        }
     }
 }
 
--- LongestIncreasingSequence/MethodTwo.cs
+++ LongestIncreasingSequence/MethodTwo.cs
@@ -9,6 +9,7 @@ namespace LongestIncreasingSequence
         public static int LongestIncreasingSequence(int[] arr)
         {
             int n = arr.Length;
+            // int[] lastNum = new int[n];
             int[] dp = new int[n];
 
             int maxLen = 0;
@@ -22,13 +23,62 @@ namespace LongestIncreasingSequence
                 }
 
                 dp[index] = num;
+                // if (index > 0) { lastNum[index] = dp[index - 1]; }
 
+                if (index == maxLen)
+                {
+                    
+                    
+                    maxLen++;
+                }
+            }
+            return maxLen;
+        }
+
+        public static int[] LongestIncreasingSequenceArray(int[] arr)
+        {
+            int n = arr.Length;
+            Dictionary<int,int> last=new Dictionary<int, int>();
+            int[] dp = new int[n];
+
+            int maxLen = 0;
+            for(int i=0;i<arr.Length;i++)
+            {
+                int num=arr[i];
+                    int index = Array.BinarySearch(dp, 0, maxLen, num);
+
+                if (index < 0)
+                {
+                    index = -(index + 1);
+                }
+
+                
+
+                //lastNum[i]=dp[index-1];
+                dp[index] = num; 
+                if (index>0){last.Add(num,dp[index-1]);}
+
+                // if (index > 0) { lastNum[index] = dp[index - 1]; }
+                
                 if (index == maxLen)
                 {
                     maxLen++;
                 }
             }
-            return maxLen;
+
+            Stack<int> result=new Stack<int>();
+            result.Push(dp[maxLen-1]);
+            var current=dp[maxLen-1];
+
+            for (int i = 1; i < maxLen; i++)
+            {
+                current=last[current];
+                result.Push(current);
+            }
+
+            // result[maxLen-1] = dp[maxLen-1];
+
+            return result.ToArray();
         }
 
     }
