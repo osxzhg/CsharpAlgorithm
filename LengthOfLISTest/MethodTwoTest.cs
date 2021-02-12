4e16609e0ab86caa5456c362ec34c19d528954b1
osxzhg <osxzhg@gmail.com>
2021-02-12 23:56:33 +1300

    add DisplayName for Theory

--- LengthOfLISTest/MethodTwoTest.cs
+++ LengthOfLISTest/MethodTwoTest.cs
@@ -8,9 +8,9 @@ namespace LengthOfLISTest
 {
     public class MethodTwoTest
     {
-        [Theory]
+        [Theory(DisplayName ="ok")]
         [InlineData(4, new int[] { 10, 9, 2, 5, 3, 7, 101, 18 })]
-        public void Test1(int expected, int[] nums)
+        public void TestOk(int expected, int[] nums)
         {
             var result = MethodTwo.LongestIncreasingSequence(nums);
             Assert.Equal(expected, result);
@@ -18,7 +18,7 @@ namespace LengthOfLISTest
         }
 
         [Theory]
-        [InlineData(8, new int[] { 100, 101, 102,105, 2,3,4,90,91,92,93,94 })]
+        [InlineData(8, new int[] { 100, 101, 102, 105, 2, 3, 4, 90, 91, 92, 93, 94 })]
         public void LargeNumberBeforeSmallNumbers(int expected, int[] nums)
         {
             var result = MethodTwo.LongestIncreasingSequence(nums);
@@ -26,8 +26,8 @@ namespace LengthOfLISTest
 
         }
         [Theory]
-        [InlineData(8, new int[] { 2, 3, 4, 90, 91,92, 93, 94 },new int[] { 100, 101, 102, 105, 2, 3, 4, 90, 91, 92, 93, 94 })]
-        public void LisArray(int dummy, int[] expected,int[] nums)
+        [InlineData(8, new int[] { 2, 3, 4, 90, 91, 92, 93, 94 }, new int[] { 100, 101, 102, 105, 2, 3, 4, 90, 91, 92, 93, 94 })]
+        public void LisArray(int dummy, int[] expected, int[] nums)
         {
             var result = MethodTwo.LongestIncreasingSequenceArray(nums);
             Assert.Equal(expected, result);
@@ -44,7 +44,7 @@ namespace LengthOfLISTest
         }
 
         [Theory]
-        [InlineData(8, new int[] { 2,6,9,11}, new int[] { 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7 })]
+        [InlineData(8, new int[] { 2, 6, 9, 11 }, new int[] { 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7 })]
         public void LisArrayWithDebug(int dummy, int[] expected, int[] nums)
         {
             var result = MethodTwo.LongestIncreasingSequenceArray(nums);
